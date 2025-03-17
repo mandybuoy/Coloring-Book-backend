0280e8092e4d01182ffa9b534d7654d713626aa5
niketjain1 <niketj2000@gmail.com>
2025-03-17 20:34:28 +0530

    Added packages

--- db.js
+++ db.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection failed", error);
